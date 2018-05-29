95b5c62dd61d8e358b2863fef1279d6c1fa82752
Yang Liu <foamliu@yeah.net>
2018-05-29 21:43:07 +0800

    more tests

--- unit_tests.py
+++ unit_tests.py
@@ -6,9 +6,15 @@ import numpy as np
 import os
 
 from data_generator import get_image, get_semantic, to_bgr
-
+from data_generator import random_choice, safe_crop
 
 class TestStringMethods(unittest.TestCase):
+    def setUp(self):
+        if not os.path.exists('temp'):
+            os.makedirs('temp')
+
+    def tearDown(self):
+        pass
 
     def test_get_semantic(self):
         name = 'SUNRGBD/kv1/NYUdata/NYU0899'
@@ -19,21 +25,20 @@ class TestStringMethods(unittest.TestCase):
         cv.imwrite('temp/test_get_semantic_image.png', image)
         cv.imwrite('temp/test_get_semantic_semantic.png', semantic)
 
-    # def test_flip(self):
-    #     image = cv.imread('fg/1-1252426161dfXY.jpg')
-    #     # print(image.shape)
-    #     alpha = cv.imread('mask/1-1252426161dfXY.jpg', 0)
-    #     trimap = generate_trimap(alpha)
-    #     x, y = random_choice(trimap)
-    #     image = safe_crop(image, x, y)
-    #     trimap = safe_crop(trimap, x, y)
-    #     alpha = safe_crop(alpha, x, y)
-    #     image = np.fliplr(image)
-    #     trimap = np.fliplr(trimap)
-    #     alpha = np.fliplr(alpha)
-    #     cv.imwrite('temp/test_flip_image.png', image)
-    #     cv.imwrite('temp/test_flip_trimap.png', trimap)
-    #     cv.imwrite('temp/test_flip_alpha.png', alpha)
+    def test_safe_crop(self):
+        name = 'SUNRGBD/kv1/NYUdata/NYU0899'
+        image, image_size = get_image(name)
+        semantic = get_semantic(name, image_size)
+        different_sizes = [(320, 320), (480, 480), (640, 640)]
+        crop_size = random.choice(different_sizes)
+
+        x, y = random_choice(image_size, crop_size)
+        image = safe_crop(image, x, y, crop_size)
+        semantic = safe_crop(semantic, x, y, crop_size)
+        semantic = to_bgr(semantic)
+        cv.imwrite('temp/test_safe_crop_image.png', image)
+        cv.imwrite('temp/test_safe_crop_semantic.png', semantic)
+
     #
     # def test_different_sizes(self):
     #     different_sizes = [(320, 320), (320, 320), (320, 320), (480, 480), (640, 640)]

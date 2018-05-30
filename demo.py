fa349690d8ee7e2c4b4f3db782ad7d89407766aa
Yang Liu <foamliu@yeah.net>
2018-05-30 16:35:37 +0800

    update

--- demo.py
+++ demo.py
@@ -10,18 +10,6 @@ from config import num_classes
 from data_generator import get_image, get_semantic
 from data_generator import random_choice, safe_crop, to_bgr
 from model import build_encoder_decoder
-from utils import draw_str
-
-
-def get_semantic(name):
-    label_test_path = 'data/semantic_test/'
-    tokens = name.split('_')
-    tokens[-1] = 'semantic_pretty.png'
-    name = '_'.join(tokens)
-    filename = os.path.join(label_test_path, name)
-    label = cv.imread(filename)
-    return label
-
 
 if __name__ == '__main__':
     img_rows, img_cols = 320, 320

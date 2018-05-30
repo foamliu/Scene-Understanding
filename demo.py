0141f932a75edcadf36b66d8e00aebdfe9dcb78d
Yang Liu <foamliu@yeah.net>
2018-05-30 16:32:22 +0800

    fix demo

--- demo.py
+++ demo.py
@@ -7,6 +7,7 @@ import keras.backend as K
 import numpy as np
 
 from config import num_classes
+from data_generator import get_image, get_semantic
 from data_generator import random_choice, safe_crop, to_bgr
 from model import build_encoder_decoder
 from utils import draw_str
@@ -26,31 +27,28 @@ if __name__ == '__main__':
     img_rows, img_cols = 320, 320
     channel = 3
 
-    model_weights_path = 'models/model.64-2.1187.hdf5'
+    model_weights_path = 'models/model.107-3.1307.hdf5'
     model = build_encoder_decoder()
     model.load_weights(model_weights_path)
 
     print(model.summary())
 
-    rgb_test_path = 'data/rgb_test/'
-    label_test_path = 'data/semantic_test/'
-    test_images = [f for f in os.listdir(rgb_test_path) if
-                   os.path.isfile(os.path.join(rgb_test_path, f)) and f.endswith('.png')]
-
-    samples = random.sample(test_images, 10)
+    filename = '{}_names.txt'.format('valid')
+    with open(filename, 'r') as f:
+        names = f.read().splitlines()
+    samples = random.sample(names, 10)
 
     for i in range(len(samples)):
-        image_name = samples[i]
-        filename = os.path.join(rgb_test_path, image_name)
-        image = cv.imread(filename)
-        label = get_semantic(image_name)
-        image_size = image.shape[:2]
+        name = samples[i]
+        image, image_size = get_image(name)
+        semantic = get_semantic(name, image_size)
         different_sizes = [(320, 320), (480, 480), (640, 640)]
         crop_size = random.choice(different_sizes)
 
         x, y = random_choice(image_size, crop_size)
         image = safe_crop(image, x, y, crop_size)
-        label = safe_crop(label, x, y, crop_size)
+        semantic = safe_crop(semantic, x, y, crop_size)
+        semantic = to_bgr(semantic)
         print('Start processing image: {}'.format(filename))
 
         x_test = np.empty((1, img_rows, img_cols, 3), dtype=np.float32)
@@ -61,14 +59,11 @@ if __name__ == '__main__':
         out = np.argmax(out, axis=2)
         out = to_bgr(out)
 
-        str_msg = 'crop_size: %s' % (str(crop_size))
-        draw_str(out, (20, 20), str_msg)
-
         if not os.path.exists('images'):
             os.makedirs('images')
 
         cv.imwrite('images/{}_image.png'.format(i), image)
         cv.imwrite('images/{}_out.png'.format(i), out)
-        cv.imwrite('images/{}_label.png'.format(i), label)
+        cv.imwrite('images/{}_semantic.png'.format(i), semantic)
 
     K.clear_session()

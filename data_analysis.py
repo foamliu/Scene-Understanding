352754cba7fcd7c168a2260320f0c0369193b60b
Yang Liu <foamliu@yeah.net>
2018-05-31 19:23:47 +0800

    update

--- data_analysis.py
+++ data_analysis.py
@@ -6,7 +6,7 @@ import numpy as np
 if __name__ == '__main__':
     counts = dict()
     for class_id in range(num_classes):
-        counts[class_id] = 0
+        counts[class_id] = 0.0
 
     filename = '{}_names.txt'.format('train')
     with open(filename, 'r') as f:
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     pb = ProgressBar(total=len(names), prefix='Analyzing train images', suffix='', decimals=3, length=50, fill='=')
 
-    total = 0
+    total = 0.0
     for i in range(len(names)):
         name = names[i]
         image, image_size = get_image(name)
@@ -22,15 +22,15 @@ if __name__ == '__main__':
         h, w = image_size
         total += h * w
 
-        local_count = 0
+        # local_count = 0
         for class_id in range(num_classes):
             mat = (semantic == class_id).astype(np.float32)
             more = np.sum(mat)
             counts[class_id] += more
-            local_count += more
+            # local_count += more
 
-        print('h * w: ' + str(h * w))
-        print('local_count: ' + str(local_count))
+        # print('h * w: ' + str(h * w))
+        # print('local_count: ' + str(local_count))
 
         pb.print_progress_bar(i + 1)
 
